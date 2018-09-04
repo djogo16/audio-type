bf70f471822cbf065273a7043e2535962213f1cd
djogo16 <kabala.frank@gmail.com>
2018-09-04 13:31:24 -0600

    add the Book model and text field to Audio model

--- django-backend/audio/models.py
+++ django-backend/audio/models.py
@@ -1,61 +1,39 @@
 from django.db import models
 
-# Create your models here.
 
+class Book(models.Model):
+
+	"""Model representing books from LibriSpeech"""
+
+	id = models.IntegerField(primary_key = True)
+	author = models.CharField(max_length = 250)
+	title = models.CharField(max_length = 250)
+	
+	def __str__(self):
+		return self.title
 class Chapter(models.Model):
 
 	"""Model representing the chapters of books from LibriSpeech"""
 
 	id = models.IntegerField(primary_key = True)
-	minutes = models.IntegerField()
+	length = models.IntegerField()
+	book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
 	title = models.CharField(max_length = 250)
-	book_title = models.CharField(max_length = 250)
-
-	# Method
-	def get_absolute_url(self):
-		"""Returns the url to access a particular instance of MyModelName."""
-		return reverse('chapter-detail-view', args=[str(self.id)])
-    
+	
 	def __str__(self):
-		"""String for representing the MyModelName object (in Admin site etc.)."""
 		return self.title
 
 class Audio(models.Model):
 
-	"""model representing the audios"""
+	"""model representing audios"""
 
 	id = models.AutoField(primary_key = True)
 	chapter_order = models.IntegerField()
-	chapter_id = models.IntegerField()
+	chapter_id = models.ForeignKey(Chapter, on_delete=models.CASCADE)
 	name = models.CharField(max_length = 250)
 	length = models.IntegerField()
-	path = models.CharField(max_length = 300)
-	audio = models.FileField(upload_to = 'audiofiles')
+	audio_file = models.FileField(upload_to = 'audiofiles')
+	text = models.TextField()
 
-	# Methods
-	def get_absolute_url(self):
-		"""Returns the url to access a particular instance of MyModelName."""
-		return reverse('audio-detail-view', args=[str(self.id)])
-    
 	def __str__(self):
-		"""String for representing the MyModelName object (in Admin site etc.)."""
 		return self.name
-
-class Transcription(models.Model):
-
-	"""Model representing the transcrption of the audio"""
-
-	id = models.AutoField(primary_key = True)
-	chapter_id = models.IntegerField()
-	name = models.CharField(max_length = 250)
-
-	# Methods
-	def get_absolute_url(self):
-		"""Returns the url to access a particular instance of MyModelName."""
-		return reverse('transcription-detail-view', args=[str(self.id)])
-    
-	def __str__(self):
-		"""String for representing the MyModelName object (in Admin site etc.)."""
-		return self.name
-
-	
