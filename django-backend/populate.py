7a1860e0844643e9286f547d8ef4f284aca25825
djogo16 <kabala.frank@gmail.com>
2018-09-04 18:25:33 -0600

    added search result drop down component

--- django-backend/populate.py
+++ django-backend/populate.py
@@ -1,20 +1,22 @@
+#Populate database with LIbrispeech ASR data(http://www.openslr.org/12/)
+
 import get_fields
-from audio.models import Audio, Chapter, Transcription
+from audio.models import Chapter, Transcription
 
-r1 = get_fields.audio_fields()
+#r1 = get_fields.audio_fields()
 r2 = get_fields.chapter_fields()
-r3 =  get_fields.transcription_fields()
+#r3 =  get_fields.transcription_fields()
 
-for x in r1:
-	record = Audio(chapter_order = int(x[0]), chapter_id = int(x[1]), name = x[2], length = int(x[4]), path = x[5])
-	record.save()
-
-
-#for x in r2:
-#	record = Chapter(id = int(x[0]), minutes = int(x[1]), title = x[2], book_title = x[3])
+#for x in r1:
+#	record = Audio(chapter_order = int(x[0]), chapter_id = int(x[1]), name = x[2], length = int(x[4]), path = x[5])
 #	record.save()
-for x in r3:
-	record = Transcription(chapter_id = int(x[0]), name = x[1])
+
+
+for x in r2:
+	record = Chapter(id = int(x[0]), minutes = int(x[2]), title = x[3], book_title = x[4])
 	record.save()
+#for x in r3:
+#	record = Transcription(chapter_id = int(x[0]), name = x[1])
+#	record.save()
 
 
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Classes from './SearchBar.css'
-const searchBar = (props) =>(
-    <div className = {Classes.SearchBar}>
-        <input type= 'text' placeholder = 'Search Book or Author to read from'/>
-        <h3>or</h3>
-        <button onClick = {props.onClick}>Random Audio</button>
-    </div>
-);
-export default searchBar;
+import SearchResult from '../SearchResult/SearchResult'
+import Aux from '../../hoc/Aux'
+    
+
+class SearchBar extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            isSearchResultVisible: true
+            
+        }
+    }
+    handleSearchOnSubmit = (event)=>{
+        this.setState({isSearchResultVisible:false})
+        event.preventDefault()
+    }
+    render(){
+        return(
+            <Aux>
+                <div className = {Classes.SearchBar}>
+                    <form  onSubmit = {this.handleSearchOnSubmit}>
+                        <input  type= 'text'  placeholder = 'Search Book or Author to read from'/>
+                    </form>
+                    <h3>or</h3>
+                    <button onClick = {this.props.onClick}>Random Audio</button>
+                </div>
+                {this.state.isSearchResultVisible ?
+                <SearchResult result = "some text"/> : null}
+                {console.log(this.state.isSearchResultVisible)}
+            </Aux>
+            
+        );
+        
+        
+        
+        
+        
+    }
+    
+}
+export default SearchBar;
+
