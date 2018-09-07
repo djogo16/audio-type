b813d209c0713a7ca206ef76660d409e41520787
djogo16 <kabala.frank@gmail.com>
2018-09-07 10:21:17 -0600

    Added URL mappings and Serializers to serve book requests

--- django-backend/audio/models.py
+++ django-backend/audio/models.py
@@ -7,7 +7,7 @@ class Book(models.Model):
 
 	id = models.IntegerField(primary_key = True)
 	author = models.CharField(max_length = 250)
-	title = models.CharField(max_length = 250)
+	title = models.CharField(max_length = 500)
 	
 	def __str__(self):
 		return self.title
@@ -17,17 +17,24 @@ class Chapter(models.Model):
 
 	id = models.IntegerField(primary_key = True)
 	length = models.IntegerField()
-	book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+	book_id = models.ForeignKey(Book, on_delete=models.CASCADE,related_name = 'chapters')
 	title = models.CharField(max_length = 250)
+	
+class Meta:
+	unique_together = ('book_id', 'title')
 	
 	def __str__(self):
 		return self.title
+
+	def __unicode__(self):
+        	return '%s' % (self.title)
+	
 
 class Audio(models.Model):
 
 	"""model representing audios"""
 
-	id = models.AutoField(primary_key = True)
+	id = models.BigIntegerField(primary_key = True)
 	chapter_order = models.IntegerField()
 	chapter_id = models.ForeignKey(Chapter, on_delete=models.CASCADE)
 	name = models.CharField(max_length = 250)
--- django-backend/audio/views.py
+++ django-backend/audio/views.py
@@ -1,15 +1,22 @@
 # audio/views.py
 from rest_framework import generics
 
-from .models import Audio
-from .serializers import AudioSerializer
+from .models import Audio, Book
+from .serializers import AudioSerializer, BookSerializer
 
 
 class ListAudio(generics.ListCreateAPIView):
-    queryset = Audio.objects.all()
-    serializer_class = AudioSerializer
+	queryset = Audio.objects.all()
+	serializer_class = AudioSerializer
 
 
 class DetailAudio(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Audio.objects.all()
-    serializer_class = AudioSerializer
+	queryset = Audio.objects.all()
+	serializer_class = AudioSerializer
+
+class SelectedBook(generics.ListCreateAPIView):
+	serializer_class = BookSerializer
+	def get_queryset(self):
+		#book_tile = self.request.query_params.get('title', None)
+		queryset = Book.objects.filter(pk=28054)
+		return queryset
